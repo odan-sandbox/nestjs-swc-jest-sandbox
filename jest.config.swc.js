c8c8af417d39ae634e99d6cf3f8d4feab24c5930
odan <odan3240@gmail.com>
2021-11-27 21:43:17 +0900

    fix variable name

--- jest.config.swc.js
+++ jest.config.swc.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('./jest.config');
 
-const config = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, 'utf-8'));
+const swcConfig = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`, 'utf-8'));
 
 module.exports = {
   ...config,
   rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
-    '^.+\\.(t|j)s$': ['@swc/jest', { ...config }],
+    '^.+\\.(t|j)s$': ['@swc/jest', { ...swcConfig }],
   },
 };
